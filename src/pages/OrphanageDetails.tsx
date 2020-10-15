18a0f7087822a029d81032a2ffb013edad737725
Rodolfo Della Violla <rodolfo.violla@gmail.com>
2020-10-15 16:17:34 -0300

    Class 4 finished

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function OrphanageDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>OrphanageDetails</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
